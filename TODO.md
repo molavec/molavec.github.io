7a34ca925dca5b000707fa3a24ee963962f0adb0
Miguel Olave <molavec@gmail.com>
2023-08-21 10:48:25 -0400

    article styling

--- TODO.md
+++ TODO.md
@@ -1,6 +1,5 @@
 * Ajustar colores de fondo
 * Ajustar colores dark mode
-* Ver si es posible añadir molavec.digitalwriters.art a github sin costo.
 * añade Google Analytics
 * añade tags
 * ajusta SEO
--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Molavec';
+export const SITE_TITLE = 'Molavec WebSite';
 export const SITE_DESCRIPTION = 'Bienvenido a mi sitio!';
