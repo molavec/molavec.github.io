0ddfb7f03ecefe0f1a98153128bf9f280a567866
Miguel Olave <molavec@gmail.com>
2023-10-20 02:12:36 -0300

    add head

--- src/consts.ts
+++ src/consts.ts
@@ -1,8 +1,8 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Web Coders';
-export const SITE_DESCRIPTION = 'Desarrollo web para escritores digitales!';
+export const SITE_TITLE = 'Molavec';
+export const SITE_DESCRIPTION = 'Fullstack Developer';
 
 // container ID
 export const GTM_CONTAINER_ID = 'GTM-5HB4756G';
